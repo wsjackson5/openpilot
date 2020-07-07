f738b40900b8714a7693862e37251cbf67ee24be
hanabi95 <youngsoo.cha@gmail.com>
2020-07-08 00:15:31 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -79,6 +79,7 @@ class CarInterface(CarInterfaceBase):
     #ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
     ret.steerRateCost = 0.4
     ret.steerActuatorDelay = 0.4  # Default delay, not measured yet
+    ret.enableGasInterceptor = 0x201 in fingerprint[0]
 
     if candidate == CAR.VOLT:
       # supports stop and go, but initial engage must be above 18mph (which include conservatism)
