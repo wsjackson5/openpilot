067cef38a6a5abe7b7b16c1deb219dcdf8d2447d
hanabi95 <youngsoo.cha@gmail.com>
2020-08-04 20:15:06 +0900

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -7,8 +7,8 @@ kegman = kegman_conf()
 CAMERA_OFFSET = float(kegman.conf['cameraOffset'])  # m from center car to camera
 
 #zorrobyte
-def mean(numbers): 
-     return float(sum(numbers)) / max(len(numbers), 1) 
+def mean(numbers):
+     return float(sum(numbers)) / max(len(numbers), 1)
 
 
 def compute_path_pinv(l=50):
@@ -65,7 +65,7 @@ class LanePlanner():
     self.lane_width = 2.85
     self.readings = []
     self.frame = 0
-    
+
     self.l_prob = 0.
     self.r_prob = 0.
 
@@ -97,33 +97,14 @@ class LanePlanner():
     self.r_poly[3] += CAMERA_OFFSET
 
     # Find current lanewidth
-    #self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
-    #current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
-    #self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
-    #speed_lane_width = interp(v_ego, [0., 31.], [2.85, 3.5])
-    #self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
-    #                  (1 - self.lane_width_certainty) * speed_lane_width
+    self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
+    current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
+    self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
+    speed_lane_width = interp(v_ego, [0., 19.], [3., 3.25])   # S.Korean Standards (urban)
+    self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
+                      (1 - self.lane_width_certainty) * speed_lane_width
 
-    #zorrobyte code
-    # Find current lanewidth
-    if self.l_prob > 0.49 and self.r_prob > 0.49:
-      self.frame += 1
-      if self.frame % 20 == 0:
-        self.frame = 0
-        current_lane_width = sorted((2.5, abs(self.l_poly[3] - self.r_poly[3]), 3.5))[1]
-        max_samples = 30
-        self.readings.append(current_lane_width)
-        self.lane_width = mean(self.readings)
-        if len(self.readings) == max_samples:
-          self.readings.pop(0)
-
-    #zorrobyte
-    # Don't exit dive
-    if abs(self.l_poly[3] - self.r_poly[3]) > self.lane_width:
-      self.r_prob = self.r_prob / interp(self.l_prob, [0, 1], [1, 3])
-    
-
-    self.d_poly = calc_d_poly(self.l_poly, self.r_poly, self.p_poly, self.l_prob, self.r_prob, self.lane_width, v_ego)
+    self.d_poly = calc_d_poly(self.l_poly, self.r_poly, self.p_poly, self.l_prob, self.r_prob, min(4.0, self.lane_width), v_ego)
 
   def update(self, v_ego, md):
     self.parse_model(md)
