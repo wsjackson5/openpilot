066f2619a65485fd78df67c846724aae836c310b
hanabi95 <youngsoo.cha@gmail.com>
2021-02-02 19:39:27 +0900

    fix

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -656,12 +656,12 @@ static void bb_ui_draw_UI(UIState *s)
 {
   //const UIScene *scene = &s->scene;
   const int bb_dml_w = 180;
-  const int bb_dml_x = (s->scene.viz_rect.x + 60);
-  const int bb_dml_y = (s->scene.viz_rect.y  + 45) + 220;
+  const int bb_dml_x = (s->viz_rect.x + 60);
+  const int bb_dml_y = (s->viz_rect.y  + 45) + 220;
 
   const int bb_dmr_w = 180;
-  const int bb_dmr_x = s->scene.viz_rect.x + s->scene.viz_rect.w - bb_dmr_w - 60;
-  const int bb_dmr_y = (s->scene.viz_rect.y + 45) + 220;
+  const int bb_dmr_x = s->viz_rect.x + s->viz_rect.w - bb_dmr_w - 60;
+  const int bb_dmr_y = (s->viz_rect.y + 45) + 220;
 
   bb_ui_draw_measures_right(s, bb_dml_x, bb_dml_y, bb_dml_w);
   bb_ui_draw_measures_left(s, bb_dmr_x, bb_dmr_y-20, bb_dmr_w);
@@ -695,7 +695,7 @@ static void ui_draw_vision_alert(UIState *s) {
                   .h = alr_h};
 
   ui_fill_rect(s->vg, rect, color);
-  ui_fill_rect(s->vg, rect, nvgLinearGradient(s->vg, rect.x, rect.y, rect.x, rect.bottom(), 
+  ui_fill_rect(s->vg, rect, nvgLinearGradient(s->vg, rect.x, rect.y, rect.x, rect.bottom(),
                                             nvgRGBAf(0.0, 0.0, 0.0, 0.05), nvgRGBAf(0.0, 0.0, 0.0, 0.35)));
 
   nvgFillColor(s->vg, COLOR_WHITE);
