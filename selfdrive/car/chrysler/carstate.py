ce35ce25f2bb1d5cc228ae5f811c0c1b773577f0
hanabi95 <youngsoo.cha@gmail.com>
2021-05-25 20:54:32 +0900

    openpilot v0.8.5 release

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -26,7 +26,6 @@ class CarState(CarStateBase):
 
     ret.brakePressed = cp.vl["BRAKE_2"]['BRAKE_PRESSED_2'] == 5  # human-only
     ret.brake = 0
-    ret.brakeLights = ret.brakePressed
     ret.gas = cp.vl["ACCEL_GAS_134"]['ACCEL_134']
     ret.gasPressed = ret.gas > 1e-5
 
