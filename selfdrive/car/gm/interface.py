642cb95957b5f9a2a0245c48dd704a9dfa13c079
hanabi95 <youngsoo.cha@gmail.com>
2021-05-28 19:17:41 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -39,14 +39,14 @@ class CarInterface(CarInterfaceBase):
 
     if LQR_enabled:
       ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.scale = 1965.0  #by 양민님
-      ret.lateralTuning.lqr.ki = 0.024
+      ret.lateralTuning.lqr.scale = 1965.0
+      ret.lateralTuning.lqr.ki = 0.04
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
-      ret.lateralTuning.lqr.k = [-110., 451.]
-      ret.lateralTuning.lqr.l = [0.33, 0.318]
-      ret.lateralTuning.lqr.dcGain = 0.00225
+      ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+      ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
+      ret.lateralTuning.lqr.dcGain = 0.002237852961363602
     elif not LQR_enabled and INDI_enabled:
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [10., 30.]
