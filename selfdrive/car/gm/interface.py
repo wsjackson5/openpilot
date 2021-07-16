c6a28c0b8e0cc57bd27df8ee2ffcf0a9b184ae78
Comma Device <device@comma.ai>
2021-07-16 07:57:51 -0700

    [tuning]

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -65,7 +65,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kdV = [0.7, 1.]  #corolla from shane fork : 0.725
       ret.lateralTuning.pid.kf = 0.00006
 
-    ret.steerRateCost = 0.4
+    ret.steerRateCost = 0.5
     ret.steerActuatorDelay = 0.34  # Default delay, not measured yet
 
     if candidate == CAR.VOLT:
