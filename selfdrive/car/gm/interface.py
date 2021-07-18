d99b54d9a3394e01dc752d3ef6977aee2e0433d0
wsjackson5 <36290024+wsjackson5@users.noreply.github.com>
2021-07-18 09:26:59 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -60,10 +60,10 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.actuatorEffectivenessV = [2.0]
     elif not LQR_enabled and not INDI_enabled:
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[10., 30.], [10., 30.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.12, 0.253], [0.009, 0.0105]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.12, 0.15], [0.009, 0.012]]
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [0.725]  #corolla from shane fork : 0.725
-      ret.lateralTuning.pid.kf = 0.00008
+      ret.lateralTuning.pid.kdV = [0.9]  #corolla from shane fork : 0.725
+      ret.lateralTuning.pid.kf = 0.000082
 
     ret.steerRateCost = 0.50
     ret.steerActuatorDelay = 0.34  # Default delay, not measured yet
